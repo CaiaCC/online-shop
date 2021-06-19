a87225871111e98b01a34d40b2c6dd7ec0950133
Caia Chuang <caiachuang@gmail.com>
2021-06-18 21:48:53 -0700

    Return a 404 page.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser');
 
 const adminRoutes = require('./routes/admin');
@@ -12,7 +13,7 @@ app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page not Found</h1>'); // NOTE: handle 404, send need to be chained the last
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html')); // NOTE: handle 404, send need to be chained the last
 })
 
 app.listen(3000);
