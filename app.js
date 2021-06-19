a484ac26c476b5ddef3ce28c6c042efc18131662
Caia Chuang <caiachuang@gmail.com>
2021-06-18 18:39:13 -0700

    Add a 404 error page.

--- app.js
+++ app.js
@@ -7,7 +7,12 @@ const shopRoutes = require('./routes/shop');
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
+
 app.use(adminRoutes);
 app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not Found</h1>'); // NOTE: handle 404, send need to be chained the last
+})
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,6 @@
 const express = require('express');
-const bodyParser = require("body-parser");
 
 const router = express.Router();
-
-router.use(bodyParser.urlencoded({extended: false}));
 
 router.get('/add-product', (req, res, next) => {
     res.send('<form action="/product" method="post"> <input type="text" name="title" /><button type="submit">Send</button></form>');
