e29eb9771febf50d7f6a3c05f49422b63ac5f3f4
Caia Chuang <caiachuang@gmail.com>
2021-06-18 21:55:34 -0700

    Use a helper function for navigation.

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const rootDir = require("./util/path");
+
 const bodyParser = require('body-parser');
 
 const adminRoutes = require('./routes/admin');
@@ -13,7 +15,7 @@ app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html')); // NOTE: handle 404, send need to be chained the last
+    res.status(404).sendFile(path.join(rootDir, "views", "404.html")); // NOTE: handle 404, send need to be chained the last
 })
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const path = require('path');
+const rooDir = require("../util/path");
 
 const router = express.Router();
 
 router.get('/add-product', (req, res, next) => {
-    res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
+    res.sendFile(path.join(rooDir, "views", "add-product.html"));
 });
 
 router.post("/add-product", (req, res, next) => {
